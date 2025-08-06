59c5302e5a730b022935f0fdcee8ed4e9cbd69d8
shh4und <alexandersznunes@gmail.com>
2025-08-06 19:11:43 +0000

    added users base structures

--- backend/internal/domain/usuario.go
+++ backend/internal/domain/usuario.go
@@ -1,1 +1,27 @@
 package domain
+
+import "gorm.io/gorm"
+
+type Usuario struct {
+	gorm.Model
+	Nome  string `json:"nome" gorm:"not null"`
+	Email string `json:"email" gorm:"not null"`
+	Senha string `json:"senha" gorm:"not null"`
+}
+
+type Profissional struct {
+	gorm.Model
+	UsuarioID            uint    `gorm:"unique;not null"`
+	Usuario              Usuario `gorm:"foreignKey:UsuarioID"`
+	Especialidade        string  `json:"especialidade"`
+	RegistroProfissional string  `json:"registro_profissional" gorm:"unique;not null"`
+}
+
+type Paciente struct {
+	gorm.Model
+	UsuarioID    uint         `gorm:"unique;not null"`
+	Usuario      Usuario      `gorm:"foreignKey:UsuarioID"`
+	Idade        int          `json:"idade" gorm:"not null"`
+	Depdendente  bool         `json:"dependente" gorm:"not null"`
+	Profissional Profissional `gorm:"foreignKey:ProfissionalID"`
+}
