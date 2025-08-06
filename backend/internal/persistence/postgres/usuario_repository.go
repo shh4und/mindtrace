c5f56c018c26834272ec0a905b78b55356ef6f12
shh4und <alexandersznunes@gmail.com>
2025-08-06 19:22:25 +0000

    init persistence layer interfaces

--- backend/internal/persistence/postgres/usuario_repository.go
+++ backend/internal/persistence/postgres/usuario_repository.go
@@ -1,1 +1,24 @@
 package postgres
+
+import (
+	"mindtrace/backend/internal/domain"
+
+	"gorm.io/gorm"
+)
+
+type UsuarioRepository interface {
+	Salvar(usuario *domain.Usuario) error
+}
+
+type gormUsuarioRepository struct {
+	db *gorm.DB
+}
+
+func NewGormUsuarioRepository(db *gorm.DB) UsuarioRepository {
+	return &gormUsuarioRepository{db: db}
+}
+
+func (r *gormUsuarioRepository) Salvar(usuario *domain.Usuario) error {
+	result := r.db.Create(usuario)
+	return result.Error
+}
