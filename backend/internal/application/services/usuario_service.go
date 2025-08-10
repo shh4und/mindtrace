07d7e43542f878fc9844b3f3ecde34de47c6c746
shh4und <alexandersznunes@gmail.com>
2025-08-10 05:36:04 +0000

    user service layer

--- backend/internal/application/services/usuario_service.go
+++ backend/internal/application/services/usuario_service.go
@@ -1,1 +1,42 @@
 package services
+
+import (
+	"mindtrace/backend/internal/domain"
+	"mindtrace/backend/internal/persistence/postgres"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UsuarioService interface {
+	Register(nome, email, senha string) (*domain.Usuario, error)
+}
+
+type usuarioService struct {
+	repo postgres.UsuarioRepository
+}
+
+func NewUsuarioService(repo postgres.UsuarioRepository) UsuarioService {
+	return &usuarioService{repo: repo}
+}
+
+func (s *usuarioService) Register(nome, email, senha string) (*domain.Usuario, error) {
+	hashSenha, err := bcrypt.GenerateFromPassword([]byte(senha), bcrypt.DefaultCost)
+
+	if err != nil {
+		return nil, err
+	}
+
+	newUsuario := &domain.Usuario{
+		Nome:  nome,
+		Email: email,
+		Senha: string(hashSenha),
+	}
+
+	err = s.repo.Save(newUsuario)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return newUsuario, nil
+}
