62ef6d202703e8bc2127d5dff1497686fa8768dc
shh4und <alexandersznunes@gmail.com>
2025-08-15 01:20:43 +0000

    atualizando dominio de usuario

--- backend/interno/dominio/usuario.go
+++ backend/interno/dominio/usuario.go
@@ -12,34 +12,42 @@ type Usuario struct {
 	Nome      string         `json:"nome" gorm:"type:varchar(255);not null"`
 	Email     string         `json:"email" gorm:"type:varchar(255);unique;not null"`
 	Senha     string         `json:"-" gorm:"type:text;not null"`
+	Contato   string         `json:"contato" gorm:"not null"`
+	Bio       string         `json:"bio" gorm:"type:text"`
+	CPF       string         `json:"cpf" gorm:"type:varchar(20);unique"`
+	CNPJ      string         `json:"cnpj" gorm:"type:varchar(20);unique"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
 
 func (Usuario) TableName() string {
-  return "usuarios"
+	return "usuarios"
 }
+
 // Profissional tem seus próprios dados e uma referência ao Usuario.
 type Profissional struct {
 	ID                   uint       `gorm:"primaryKey"`
 	UsuarioID            uint       `json:"-" gorm:"unique;not null"`
 	Usuario              Usuario    `json:"usuario" gorm:"foreignKey:UsuarioID"`
 	Especialidade        string     `json:"especialidade" gorm:"type:varchar(255)"`
-	RegistroProfissional string     `json:"registro_profissional" gorm:"type:varchar(100);unique;not null"`
+	RegistroProfissional string     `json:"registro_profissional" gorm:"type:varchar(12);unique;not null"`
 	Pacientes            []Paciente `json:"pacientes" gorm:"many2many:profissional_paciente;"`
 	CreatedAt            time.Time  `json:"created_at"`
 	UpdatedAt            time.Time  `json:"updated_at"`
 }
 
 func (Profissional) TableName() string {
-  return "profissionais"
+	return "profissionais"
 }
+
 // Paciente tem seus próprios dados e uma referência ao Usuario.
 type Paciente struct {
 	ID                   uint           `gorm:"primaryKey"`
 	UsuarioID            uint           `json:"-" gorm:"unique;not null"`
 	Usuario              Usuario        `json:"usuario" gorm:"foreignKey:UsuarioID"`
+	Idade                int8           `json:"idade" gorm:"not null"`
+	EhDependente         bool           `json:"eh_dependente" gorm:"not null"`
 	DataInicioTratamento *time.Time     `json:"data_inicio_tratamento"`
 	HistoricoSaude       string         `json:"historico_saude" gorm:"type:text"`
 	Profissionais        []Profissional `json:"profissionais" gorm:"many2many:profissional_paciente;"`
@@ -48,7 +56,7 @@ type Paciente struct {
 }
 
 func (Paciente) TableName() string {
-  return "pacientes"
+	return "pacientes"
 }
 
 // ResponsavelLegal representa a entidade de um responsável por um paciente.
@@ -65,6 +73,5 @@ type ResponsavelLegal struct {
 }
 
 func (ResponsavelLegal) TableName() string {
-  return "responsaveis_legais"
+	return "responsaveis_legais"
 }
-
