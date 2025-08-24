6dd23619d74ec2f2fb6d7000520e1d6dfb74610b
shh4und <alexandersznunes@gmail.com>
2025-08-24 01:46:45 +0000

    minor fix

--- backend/interno/dominio/registro_humor.go
+++ backend/interno/dominio/registro_humor.go
@@ -9,10 +9,10 @@ type RegistroHumor struct {
 	ID               uint      `gorm:"primaryKey"`
 	PacienteID       uint      `json:"-" gorm:"not null"`
 	Paciente         Paciente  `json:"paciente" gorm:"foreignKey:PacienteID"`
-	NivelHumor       int16     `json:"nivel_humor" gorm:"not null;check:nivel >= 1 AND nivel <= 7"`
-	HorasSono        int16     `json:"horas_sono" gorm:"not null;check:horas_sono >= 0 and horas_sono <= 24"`
-	NivelStress      int16     `json:"nivel_stress" gorm:"not null;check:horas_sono >= 0 and horas_sono <= 10"`
-	NivelEnergia     int16     `json:"nivel_energia" gorm:"not null;check:horas_sono >= 0 and horas_sono <= 10"`
+	NivelHumor       int16     `json:"nivel_humor" gorm:"not null;check:nivel >= 1 AND nivel <= 5"`
+	HorasSono        int16     `json:"horas_sono" gorm:"not null;check:horas_sono >= 0 and horas_sono <= "`
+	NivelEnergia     int16     `json:"nivel_energia" gorm:"not null;check:nivel_energia >= 1 and nivel_energia <= 10"`
+	NivelStress      int16     `json:"nivel_stress" gorm:"not null;check:nivel_stress >= 1 and nivel_stress <= 10"`
 	AutoCuidado      []string  `json:"auto_cuidado" gorm:"not null"`
 	Observacoes      string    `json:"observacoes" gorm:"type:text"`
 	DataHoraRegistro time.Time `json:"data_hora_registro" gorm:"not null;default:now()"`
