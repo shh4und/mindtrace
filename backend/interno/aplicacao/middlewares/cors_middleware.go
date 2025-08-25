5ba9568ab7aee8c5e7246ec8bbb89661322e3c7f
shh4und <alexandersznunes@gmail.com>
2025-08-25 03:57:09 +0000

    adicao de origem permitida

--- backend/interno/aplicacao/middlewares/cors_middleware.go
+++ backend/interno/aplicacao/middlewares/cors_middleware.go
@@ -10,7 +10,7 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		// IMPORTANTE: Em produção, troque "*" por a URL exata do seu frontend.
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "http://localhost:8081"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "http://localhost:8081", "http://localhost:8000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
