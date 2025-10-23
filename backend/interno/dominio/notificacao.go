b2a8ef388fc31cb74d6bb44e6daf454a4e4744b4
shh4und <alexandersznunes@gmail.com>
2025-10-23 16:28:52 -0300

    atualizando campos

--- backend/interno/dominio/notificacao.go
+++ backend/interno/dominio/notificacao.go
@@ -10,7 +10,6 @@ type Notificacao struct {
 	UsuarioID uint      `json:"-" gorm:"not null"`
 	Usuario   Usuario   `json:"usuario" gorm:"foreignKey:UsuarioID;constraint:OnDelete:CASCADE"`
 	AlertaID  *uint     `json:"-"` // Ponteiro para permitir valor NULL
-	Alerta    *Alerta   `json:"alerta,omitempty" gorm:"foreignKey:AlertaID;constraint:OnDelete:CASCADE"`
 	Conteudo  string    `json:"conteudo" gorm:"type:text;not null"`
 	Status    string    `json:"status" gorm:"type:varchar(50);not null;default:'Não lida'"`
 	DataEnvio time.Time `json:"data_envio" gorm:"not null;default:CURRENT_TIMESTAMP"`
