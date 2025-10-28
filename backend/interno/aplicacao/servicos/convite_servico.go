9a50792e4078f66f8ba8046b557ab47d8d3cd407
shh4und <alexandersznunes@gmail.com>
2025-10-27 22:38:56 -0300

    servico de convite utilizando regras de dominio

--- backend/interno/aplicacao/servicos/convite_servico.go
+++ backend/interno/aplicacao/servicos/convite_servico.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"errors"
+	"mindtrace/backend/interno/aplicacao/dtos"
+	"mindtrace/backend/interno/aplicacao/mappers"
 	"mindtrace/backend/interno/dominio"
 	"mindtrace/backend/interno/persistencia/repositorios"
 	"time"
@@ -19,7 +21,7 @@ var (
 
 // ConviteServico define os metodos para gerenciamento de convites
 type ConviteServico interface {
-	GerarConvite(userID uint) (*dominio.Convite, error)
+	GerarConvite(userID uint) (*dtos.ConviteDTOOut, error)
 	VincularPaciente(userID uint, token string) error
 }
 
@@ -40,35 +42,53 @@ func NovoConviteServico(db *gorm.DB, cr repositorios.ConviteRepositorio, ur repo
 }
 
 // GerarConvite gera um novo convite para o profissional
-func (s *conviteServico) GerarConvite(userID uint) (*dominio.Convite, error) {
-	profissional, err := s.usuarioRepositorio.BuscarProfissionalPorUsuarioID(s.db, userID)
-	if err != nil {
-		return nil, ErrPerfilNaoEncontrado
-	}
+func (s *conviteServico) GerarConvite(userID uint) (*dtos.ConviteDTOOut, error) {
+	var conviteGerado *dominio.Convite
+	err := s.db.Transaction(func(tx *gorm.DB) error {
+		profissional, err := s.usuarioRepositorio.BuscarProfissionalPorUsuarioID(tx, userID)
+		if err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return dominio.ErrUsuarioNaoEncontrado
+			}
+			return err
+		}
 
-	tokenBytes := make([]byte, 16)
-	if _, err := rand.Read(tokenBytes); err != nil {
-		return nil, err
-	}
-	token := hex.EncodeToString(tokenBytes)
+		// token criptografado aleatoriamente
+		tokenBytes := make([]byte, 16)
+		if _, err := rand.Read(tokenBytes); err != nil {
+			return err
+		}
+		// de bytes para string
+		token := hex.EncodeToString(tokenBytes)
 
-	convite := &dominio.Convite{
-		ProfissionalID: profissional.ID,
-		Token:          token,
-		DataExpiracao:  time.Now().Add(24 * time.Hour),
-		Usado:          false,
-	}
+		convite := &dominio.Convite{
+			ProfissionalID: profissional.ID,
+			Token:          token,
+			DataExpiracao:  time.Now().Add(24 * time.Hour),
+			Usado:          false,
+		}
 
-	if err := s.conviteRepositorio.CriarConvite(s.db, convite); err != nil {
-		return nil, err
-	}
+		// Validar convite antes de criar
+		if err := convite.Validar(); err != nil {
+			return err
+		}
 
-	return convite, nil
+		if err := s.conviteRepositorio.CriarConvite(tx, convite); err != nil {
+			return err
+		}
+
+		conviteGerado = convite
+		return nil
+
+	})
+
+	return mappers.ConviteParaDTOOut(conviteGerado), err // err = nil
 }
 
 // VincularPaciente vincula um paciente a um profissional usando um token de convite
 func (s *conviteServico) VincularPaciente(userID uint, token string) error {
 	return s.db.Transaction(func(tx *gorm.DB) error {
+		// Buscar convite pelo token recebido
 		convite, err := s.conviteRepositorio.BuscarConvitePorToken(tx, token)
 		if err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
@@ -76,22 +96,32 @@ func (s *conviteServico) VincularPaciente(userID uint, token string) error {
 			}
 			return err
 		}
-
-		if time.Now().After(convite.DataExpiracao) {
-			return ErrConviteExpirado
+		// Validar se o convite recebido esta valido com os metodos de dominio
+		if !convite.EstaValido() {
+			if convite.EstaExpirado() {
+				return dominio.ErrConviteExpirado
+			}
+			if convite.JaFoiUtilizado() {
+				return dominio.ErrConviteJaUtilizado
+			}
 		}
-
+		// Busca paciente
 		paciente, err := s.usuarioRepositorio.BuscarPacientePorUsuarioID(tx, userID)
 		if err != nil {
-			return ErrPerfilNaoEncontrado
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return dominio.ErrUsuarioNaoEncontrado
+			}
+			return err
 		}
 
+		// Vincular paciente ao profissional (insere na tabela de associacao profissionais_pacientes)
 		profissionalShell := &dominio.Profissional{ID: convite.ProfissionalID}
-
 		if err := tx.Model(profissionalShell).Association("Pacientes").Append(paciente); err != nil {
 			return err
 		}
 
-		return s.conviteRepositorio.MarcarConviteComoUsado(tx, convite.ID, paciente.ID)
+		convite.UtilizarConvite(paciente.ID)
+
+		return s.conviteRepositorio.MarcarConviteComoUsado(tx, convite)
 	})
 }
